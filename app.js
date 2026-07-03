3d7b6fc5c64c0ea33369b6c1703114128803e730
Rafsun <mdrafsunjani76@gmail.com>
2026-07-03 22:59:15 +0600

    done

---
+++ app.js
@@ -0,0 +1,12 @@
+let interviewArray = [];
+let rejectedArray = [];
+let currentStatus = 'all-filter-btn'
+//  btn selectors
+const buttons = document.querySelectorAll('#nav-btn-container button');
+const allBtn = document.getElementById('all-filter-btn');
+const interviewBtn = document.getElementById('interview-filter-btn');
+const rejectedBtn = document.getElementById('rejected-filter-btn');
+
+
+
+
